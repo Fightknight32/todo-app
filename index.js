586ece34f08cc26ef62e9f2d19b0420a68317025
Fightknight32 <97274625+Fightknight32@users.noreply.github.com>
2023-05-07 21:51:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const btn = document.getElementById('button')
+const pp = document.getElementById('something')
+
+btn.addEventListener("click", () => {
+    console.log(input.value)
+    if (input.value != 0) {
+        list.innerHTML += ` <div class="card mt-1 p-2 flex-row">
+                            <p class="col-10">${input.value}</p>
+                            <button onclick="this.parentElement.style.display = 'none';" class="btn btn-close ms-auto"></button>
+                            </div>`
+
+    }
+})
+
